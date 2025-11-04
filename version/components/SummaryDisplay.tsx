8e3c435710ea98112b5b7736ac5b2281822e8780
androidpt7 <elton.duarte6@gmail.com>
2025-11-04 17:48:16 +0000

    Add files via upload

---
+++ version/components/SummaryDisplay.tsx
@@ -0,0 +1,2 @@
+// Este ficheiro foi intencionalmente deixado em branco.
+// Todo o código foi movido para um script inline no `index.html`.
---
+++ version/index.tsx
@@ -0,0 +1,2 @@
+// Este ficheiro foi intencionalmente deixado em branco.
+// Todo o código foi movido para um script inline no `index.html` para facilitar a publicação em serviços como o Netlify Drop.
